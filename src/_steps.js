0f785da76f57f3301a02a6682bd40062fefd516a
proalamin-247 <pro.alamin247@gmail.com>
2022-04-06 02:22:21 +0600

    firebase all setup complete and signin button show login pop up

---
+++ src/_steps.js
@@ -0,0 +1,14 @@
+/*
+*Steps to use Firbase*
+1. creat a project on condole.firebase.google.com
+2. install firebase
+3. resister web app in firebase
+4. copy firebase init with confiq from firebase projects setting into a file firebase.init.js
+5. export default app
+6. import firebase init.js into your app.js
+7. import getAuth from firebase/auth and create auth = getAuth(app)
+8. turn on google authentication (firebase > authentication > enable google sign in)
+9. create google provider
+10. sign 
+
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyASXAbVRoCeAT-fD6HhnC-r1Yo_FcGHAMg",
+    authDomain: "simple-firbase-authentic-6a77b.firebaseapp.com",
+    projectId: "simple-firbase-authentic-6a77b",
+    storageBucket: "simple-firbase-authentic-6a77b.appspot.com",
+    messagingSenderId: "1099134218093",
+    appId: "1:1099134218093:web:53368aa65da5bff2df7464"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
