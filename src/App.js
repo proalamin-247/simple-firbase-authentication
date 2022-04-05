5d56301c7c41ce2b924d42d38bc10914f8bd4863
proalamin-247 <pro.alamin247@gmail.com>
2022-04-06 02:57:42 +0600

    after login show user data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
 import app from './firebase.init';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from 'react';
 
 
 const auth = getAuth(app)
 
 function App() {
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
 
   const handaleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch((error) => {
         console.log('error khaichow', error);
       })
-
   }
+
   return (
     <div className="App">
       <button onClick={handaleGoogleSignIn}>Google Sign In</button>
+      <h2>Name: {user.displayName}</h2>
+      <p>Email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
--- src/_steps.js
+++ src/_steps.js
@@ -1,14 +1,15 @@
 /*
-*Steps to use Firbase*
+*Steps to use Firebase*
 1. creat a project on condole.firebase.google.com
 2. install firebase
 3. resister web app in firebase
 4. copy firebase init with confiq from firebase projects setting into a file firebase.init.js
-5. export default app
-6. import firebase init.js into your app.js
-7. import getAuth from firebase/auth and create auth = getAuth(app)
+5. export default app firebase.init.js
+6. import getAuth from firebase/auth and create auth = getAuth(app) in app.js
+7. import firebase init.js into your app.js
 8. turn on google authentication (firebase > authentication > enable google sign in)
 9. create google provider
-10. sign 
+10. use SignInWithPopup and pass auth and provide
+11. handle .then(If successful) and catch error (if error)
 
 */
