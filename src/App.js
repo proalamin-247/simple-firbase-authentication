bc33c6bc2a4317cf5cf1734b9c77e97619f5cca8
proalamin-247 <pro.alamin247@gmail.com>
2022-04-06 03:36:13 +0600

    github auth added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from 'react';
 
 
@@ -8,10 +8,12 @@ const auth = getAuth(app)
 
 function App() {
   const [user, setUser] = useState({})
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
 
   const handaleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         const user = result.user;
         setUser(user);
@@ -22,9 +24,44 @@ function App() {
       })
   }
 
+  const handaleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+        console.log('successful sign out')
+      }).catch((error) => {
+        setUser({});
+        console.log('sign out failed')
+      });
+
+  }
+
+  const handleGithubSignIn=()=>{
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        // const credential = GithubAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+    .catch(error=>{
+      setUser({});
+      console.log('error', error)
+    })
+  }
+
   return (
     <div className="App">
-      <button onClick={handaleGoogleSignIn}>Google Sign In</button>
+      {/* {condition ? true : flase} */}
+      {
+        user.uid ? <button onClick={handaleGoogleSignOut}>Sign out</button>
+        :
+        <>
+            <button onClick={handaleGoogleSignIn}>Google Sign In</button>
+            <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </>
+      }
       <h2>Name: {user.displayName}</h2>
       <p>Email: {user.email}</p>
       <img src={user.photoURL} alt="" />
